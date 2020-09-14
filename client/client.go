19f9c2e2abb1f5626986670d8c21978e4a0cdbc5
Daniel Koch <daniel@dkprog.com>
2020-09-14 09:18:32 -0300

    Client created.
    Using localhost's TCP ECHO service provided by inetd.

---
+++ client/client.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:7")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected into", conn.RemoteAddr().String())
+	defer conn.Close()
+	codes := []string{"alpha", "bravo", "charlie", "delta", "echo"}
+	for _, code := range codes {
+		conn.SetDeadline(time.Now().Add(3 * time.Second))
+
+		n, err := conn.Write([]byte(code))
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(">>", code)
+
+		buf := make([]byte, n)
+		n, err = conn.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("<<", code)
+	}
+}
